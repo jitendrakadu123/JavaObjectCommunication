0fa2042e6facf6871a66a57fa6bb83d2d9e89d25
jitendra123 <jittukadu@gmail.com>
2015-07-28 12:01:18 -0700

    Initial commit

---
+++ src/DisplayMessage.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Jitendra Kadu on 7/28/2015.
+ */
+public class DisplayMessage {
+
+    public void displayMessage(String msgToDisp){
+        System.out.println(msgToDisp);
+    }
+}
---
+++ src/MainClass.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jitendra Kadu on 7/28/2015.
+ */
+public class MainClass {
+
+    public static void main(String args){
+
+        String message = "Jitendra kadu";
+        DisplayMessage sendMessageClass = new DisplayMessage();
+        sendMessageClass.displayMessage(message);
+    }
+}
